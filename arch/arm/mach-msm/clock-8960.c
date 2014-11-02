51010eb070ea168c77d9a1b490c26ddfe83bb814
zeroblade1984 <zeroblade1984@gmail.com>
2014-11-03 00:57:22 +0800

    Add option for GPU Overclock
    Signed-off-by: zeroblade1984 <zeroblade1984@gmail.com>

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3585,7 +3585,11 @@ static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-	[VDD_DIG_HIGH]    = 400000000
+#ifdef CONFIG_GPU_OVERCLOCK
+	[VDD_DIG_HIGH]    = 450000000
+#else
+ 	[VDD_DIG_HIGH]    = 400000000
+#endif
 };
 
 static unsigned long fmax_gfx3d_8930[VDD_DIG_NUM] = {
@@ -6624,6 +6628,11 @@ static void __init reg_init(void)
 	}
 
 	if (cpu_is_apq8064() || cpu_is_apq8064aa()) {
+#ifdef CONFIG_GPU_OVERCLOCK
+		pll15_config.l = 0x21 | BVAL(31, 7, 0x620);
+		pll15_config.m = 0x1;
+		pll15_config.n = 0x3;
+#endif
 		/* Program PLL15 to 975MHz with ref clk = 27MHz */
 		configure_sr_pll(&pll15_config, &pll15_regs, 0);
 	} else if (cpu_is_apq8064ab()) {
